a3896fc8ef4da77c5c984054b4470e68b8e81480
DESKTOP-M172E8C\sylve <sylvester127@naver.com>
2020-10-23 15:49:08 +0900

    style: Remove unnecessary comments

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-//@SpringBootApplication(scanBasePackages={"com.example.demo.repository"})
 @SpringBootApplication
-//@MapperScan(value= {"com.example.demo.repository"})
 public class DemoApplication {
-	
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
