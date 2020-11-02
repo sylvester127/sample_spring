5399c9f3922f0fbd996c16ed8908972ca8cca46a
DESKTOP-M172E8C\sylve <sylvester127@naver.com>
2020-11-02 10:32:31 +0900

    refactor: Rename class and merge class and Change Mepping Annotation

---
+++ src/main/java/com/example/demo/controller/NoticeController.java
@@ -0,0 +1,89 @@
+package com.example.demo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.domain.FileVO;
+import com.example.demo.domain.NoticeVO;
+import com.example.demo.service.NoticeService;
+
+@Controller
+@RequestMapping("notices")
+public class NoticeController {
+
+	@Autowired
+	NoticeService service;
+
+	@GetMapping("")
+	private String list(Model model, HttpSession session) throws Exception {
+		String id = (String) session.getAttribute("id");
+
+		model.addAttribute("id", id);
+		model.addAttribute("list", service.getList());
+
+		return "notice/list";
+	}
+
+	@GetMapping("/reg")
+	private String reg() {
+		return "notice/reg";
+	}
+
+	@PostMapping("/reg")
+	private String reg(NoticeVO notice, FileVO file, HttpServletRequest request, @RequestPart MultipartFile realfile)
+			throws Exception {
+		if (file.isEmpty()) { // 업로드할 파일이 없을 시
+			service.addNotice(notice, request); // 게시글 insert
+		} else {
+			service.addNotice(notice, request); // 게시글 insert
+			service.fileInsertService(notice, file, realfile); // 파일 insert
+		}
+		return "redirect:/list";
+	}
+
+	@GetMapping("{boardNum}")
+	private String detail(@PathVariable int boardNum, Model model, HttpServletRequest request) throws Exception {
+		model.addAttribute("detail", service.getNotice(boardNum));
+		model.addAttribute("files", service.fileDetailService(boardNum, request, null));
+		service.updateViewCntService(boardNum);
+		return "notice/detail";
+	}
+
+	@PatchMapping("{boardNum}") // 게시글 수정폼 호출
+	private String update(@PathVariable int boardNum, Model model) throws Exception {
+		model.addAttribute("detail", service.getNotice(boardNum));
+		return "notice/update";
+	}
+
+	@GetMapping("updateProc")
+	private String updateProc(/* @ModelAttribute */ NoticeVO board, HttpServletRequest request) throws Exception {
+		service.regNotice(board, request);
+		return "redirect:/detail/" + request.getParameter("boardNum");
+	}
+
+	@DeleteMapping("{boardNum}")
+	private String delete(@PathVariable int boardNum) throws Exception {
+		service.boardDeleteService(boardNum);
+		return "redirect:/list";
+	}
+
+	@GetMapping("/fileDown/{boardNum}")
+	private void fileDown(@PathVariable int boardNum, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		request.setCharacterEncoding("UTF-8");
+		service.fileDetailService(boardNum, request, response);
+	}
+}
